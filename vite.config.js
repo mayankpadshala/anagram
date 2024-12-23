fb71a95fc6ab8e285e7e0e5120fded7c41834183
Mayank Padshala <mayankpadshala3012@gmail.com>
2024-12-23 00:17:40 -0800

    Fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 export default defineConfig({
-    base: "/anagram",
+    base: "/anagram/",
     plugins: [react()],
     resolve: {
         alias: {
